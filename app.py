c720c27705b6446b166573473873ab470d268930
Chidi-Bernard Emmanuel <89081878+bernard-rr@users.noreply.github.com>
2023-09-26 11:22:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import streamlit as st
 from combine_pdf import combine_pdfs
 from split_pdf import split_pdfs
+import urllib.parse
 
 def main():
     st.title("PDF Master")
+
+    st.markdown("[Read the instructions here](https://github.com/bernard-rr/pdf_master#usage)")
+
+    email = "bernardchidi5@gmail.com"
+    subject = urllib.parse.quote("Feedback on the PDF Master App")
+    feedback_link = f"[Send me feedback](mailto:{email}?subject={subject})"
+    st.markdown(feedback_link)
 
     action = st.radio("Select an action:", ["Split PDF", "Combine PDF"])
 
