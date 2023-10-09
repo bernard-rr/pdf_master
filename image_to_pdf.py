e0bdb98ee28f42d28b4b962402827445d4b304bc
Chidi-Bernard Emmanuel <89081878+bernard-rr@users.noreply.github.com>
2023-10-09 09:50:54 +0000

    update

--- image_to_pdf.py
+++ image_to_pdf.py
@@ -4,45 +4,39 @@ from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 import tempfile
 import zipfile
-from utils import get_binary_file_downloader_html
 import os
 
 def image_to_pdf(uploaded_image, output_pdf_path):
-    # Save the uploaded image to a temporary file
     with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as temp_image:
         temp_image.write(uploaded_image.read())
 
-    # Open the temporary image using PIL
     img = Image.open(temp_image.name)
-
-    # Create a PDF canvas with the same dimensions as the image
     c = canvas.Canvas(output_pdf_path, pagesize=img.size)
-
-    # Draw the image on the PDF canvas
     c.drawImage(temp_image.name, 0, 0, img.width, img.height)
-
-    # Save the PDF file
     c.save()
 
 def convert_images_to_pdf(images):
     pdf_files = []
-
-    # Create a temporary directory for storing PDFs
-    temp_dir = tempfile.mkdtemp()
-
-    for image in images:
-        image_name = image.name
-        pdf_name = os.path.splitext(image_name)[0] + ".pdf"
-        output_pdf_path = os.path.join(temp_dir, pdf_name)
-
-        # Convert each image to PDF
-        image_to_pdf(image, output_pdf_path)
+    
+    for uploaded_image in images:
+        image_name = uploaded_image.name
+        output_pdf_path = f"{os.path.splitext(image_name)[0]}.pdf"
+        image_to_pdf(uploaded_image, output_pdf_path)
         pdf_files.append(output_pdf_path)
 
-    # Create a zip folder and add the PDFs to it
+    temp_dir = tempfile.mkdtemp()
     zip_folder = os.path.join(temp_dir, "converted_pdfs.zip")
+
     with zipfile.ZipFile(zip_folder, 'w', zipfile.ZIP_DEFLATED) as zip_file:
         for pdf_file in pdf_files:
             zip_file.write(pdf_file, os.path.basename(pdf_file))
 
     return zip_folder
+
+if __name__ == "__main__":
+    uploaded_images = st.file_uploader("Choose images to convert to PDF", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
+    
+    if uploaded_images:
+        zip_folder = convert_images_to_pdf(uploaded_images)
+        st.success("Images successfully converted to PDFs. Click the link below to download the ZIP:")
+        st.markdown(get_binary_file_downloader_html(zip_folder, 'Download ZIP'), unsafe_allow_html=True)
