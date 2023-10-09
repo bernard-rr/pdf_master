c405b5d8176244551122dfce000fee539ee74fcf
Chidi-Bernard Emmanuel <89081878+bernard-rr@users.noreply.github.com>
2023-10-09 09:09:06 +0000

    update

--- image_to_pdf.py
+++ image_to_pdf.py
@@ -2,22 +2,26 @@ import streamlit as st
 from PIL import Image
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
+import tempfile
 
-def image_to_pdf(input_image_path, output_pdf_path):
-    # Open the image using PIL
-    img = Image.open(input_image_path)
+def image_to_pdf(uploaded_image, output_pdf_path):
+    # Save the uploaded image to a temporary file
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".jpg") as temp_image:
+        temp_image.write(uploaded_image.read())
+
+    # Open the temporary image using PIL
+    img = Image.open(temp_image.name)
 
     # Create a PDF canvas with the same dimensions as the image
     c = canvas.Canvas(output_pdf_path, pagesize=img.size)
 
     # Draw the image on the PDF canvas
-    c.drawImage(input_image_path, 0, 0, img.width, img.height)
+    c.drawImage(temp_image.name, 0, 0, img.width, img.height)
 
     # Save the PDF file
     c.save()
 
 def convert_image_to_pdf():
-    st.write("Convert an image to PDF")
 
     uploaded_image = st.file_uploader("Choose an image to convert to PDF", type=["jpg", "jpeg", "png"])
 
